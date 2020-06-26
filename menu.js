791c916ca79e715d68b064129544f2730c85fd83
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2020-06-26 12:51:03 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+function dropdown(){
+    if (document.getElementById("menu-drop").style.visibility == "hidden"){
+        document.getElementById("menu-drop").style.visibility="visible"
+    }else{
+        document.getElementById("menu-drop").style.visibility="hidden"
+    }
+}
